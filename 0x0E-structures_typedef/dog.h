f434e2efda931a537eb7bb12e1e8b7582e0bd185
fahde93 <ayoubfahde88@gmail.com>
2023-08-14 12:57:47 -0700

    a

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,4 +19,19 @@ struct dog
 	char *owner;
 };
 
-#endif
+/**
+ * dog_t - typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+void free_dog(dog_t *d);
+
+
+#endif /* DOG_H */
+	  
