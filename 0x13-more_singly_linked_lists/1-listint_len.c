412a5dfbd265a2f590ac1178024060b522c12571
fahde93 <ayoubfahde88@gmail.com>
2023-08-28 15:16:19 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,23 +2,21 @@
 
 
 /**
- *  *listint_len - Returns the number of elements
- *   *in a linked listint_t list
- *    *@h: A pointer to the head of the list
- *     *
- *      *
- *       *Return: the number of elements in the listint_t list
- *        */
+ * listint_len - Returns the number of elements
+ * in a linked listint_t list
+ * @h: A pointer to the head of the list
+ * Return: the number of elements in the listint_t list
+ */
 
 size_t listint_len(const listint_t *h)
 {
-		size_t nodes = 0;
+	size_t nodes = 0;
 
-			while (h)
-					{
-								nodes++;
-										h = h->next;
-											}
-				return (nodes);
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
 }
 
